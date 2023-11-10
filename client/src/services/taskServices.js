0c6b9e6ca5d228449909e9b6ba00611d71752524
Nithya2303 <122733926+Nithya2303@users.noreply.github.com>
2023-11-10 12:41:29 +0530

    update

--- client/src/services/taskServices.js
+++ client/src/services/taskServices.js
@@ -1,18 +1,19 @@
 import axios from "axios";
-const apiUrl = "http://localhost:5000/api/tasks";
 
-export function getTasks() {
-    return axios.get(apiUrl);
-}
+const apiUrl = "https://todo-project-server.vercel.app/api/tasks";
 
-export function addTask(task) {
-    return axios.post(apiUrl, task);
-}
+export const getTasks = () => {
+  return axios.get(apiUrl);
+};
 
-export function updateTask(id, task) {
-    return axios.put(apiUrl + "/" + id, task);
-}
+export const addTask = (task) => {
+  return axios.post(apiUrl, task);
+};
 
-export function deleteTask(id) {
-    return axios.delete(apiUrl + "/" + id);
-}
+export const updateTask = (id, task) => {
+  return axios.put(`${apiUrl}/${id}`, task);
+};
+
+export const deleteTask = (id) => {
+  return axios.delete(`${apiUrl}/${id}`);
+};
